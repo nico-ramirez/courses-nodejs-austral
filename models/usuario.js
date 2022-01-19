cca2e638dfd82773c106ba3d3ddbb19abb487e64
Nicolas <nicolas.ramirez.28@hotmail.com>
2022-01-18 22:50:37 -0300

    user model for authentication

--- models/usuario.js
+++ models/usuario.js
@@ -2,9 +2,50 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Reserva = require('./reserva');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const validatreEmail = function(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 var usuarioSchema = new Schema({
-    nombre: String
+    nombre: {
+        typ: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        typ: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        validate: [validatreEmail, 'Por favor, ingrese un email valido'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/]
+    },
+    nombre: {
+        typ: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+usuarioSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+usuarioSchema.mathods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
 
 usuarioSchema.statics.createInstance = function(nombre){
     return new this({
@@ -24,11 +65,6 @@ usuarioSchema.statics.all = function(callback) {
 usuarioSchema.statics.add = function(user, callback){
     this.create(user, callback);
 }
-
-// usuarioSchema.methods.save = function(name, callback){
-//     var usuario = new Usuario({nombre: name});
-//     usuario.save();
-// }
 
 usuarioSchema.statics.findByName = function(name, callback){
     return this.findOne({nombre: name}, callback);
